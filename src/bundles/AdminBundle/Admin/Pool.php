74b29de646086354aaa0b8772e1414e2b71898d3
gzilt <viktore.zara@gmail.com>
2016-08-04 16:25:39 +0800

    Initial import of RzAdminBundle

---
+++ src/bundles/AdminBundle/Admin/Pool.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bruery\AdminBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Pool as BasePool;
+
+class Pool extends BasePool
+{
+    public function setOption($name, $value)
+    {
+        $this->options[$name] = $value;
+    }
+}

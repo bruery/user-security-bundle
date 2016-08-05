99969c6384a66357972dc85b6bc085f221695bd2
gzilt <viktore.zara@gmail.com>
2016-08-05 16:18:46 +0800

    Fix all copyright typo on admin-bundle

--- Resources/public/ckeditor-templates/templates/rz-default.js
+++ Resources/public/ckeditor-templates/templates/rz-default.js
@@ -1,6 +1,10 @@
-﻿/**
- * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+﻿/*
+ * This file is part of the Bruery Platform.
+ *
+ * (c) Viktore Zara <viktore.zara@gmail.com>
+ * (c) Mell Zamora <mellzamora@outlook.com>
+ *
+ * Copyright (c) 2016. For the full copyright and license information, please view the LICENSE  file that was distributed with this source code.
  */
 
 // Register a templates definition set named "default".

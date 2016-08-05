2599adfe74615f636d79a60f99c152086133dae0
gzilt <viktore.zara@gmail.com>
2016-08-05 14:36:39 +0800

    Added code copyright header.
    Confirm with the coding standard.

--- src/bundles/AdminBundle/AdminBundle.php
+++ src/bundles/AdminBundle/AdminBundle.php
@@ -1,6 +1,14 @@
 <?php
 
 namespace Bruery\AdminBundle;
+/**
+ * This file is part of the Bruery Platform.
+ *
+ * (c) Viktore Zara <viktore.zara@gmail.com>
+ * (c) Mell Zamorw <mellzamora@outlook.com>
+ *
+ * Copyright (c) 2016. For the full copyright and license information, please view the LICENSE  file that was distributed with this source code.
+ */
 
 use Bruery\AdminBundle\DependencyInjection\Compiler\OverrideCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;

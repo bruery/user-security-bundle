2be2238da7ac3f15473e4f7f25a43c14e95638a0
gzilt <viktore.zara@gmail.com>
2016-08-05 15:17:33 +0800

    Squashed 'src/bundles/AdminBundle/' changes from 0afe33a..74826a9
    74826a9 Added code copyright header.
    
    git-subtree-dir: src/bundles/AdminBundle
    git-subtree-split: 74826a98584cfa167fb9c44730ad8d05e37f0687

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,15 +1,18 @@
 <?php
+/**
+ * This file is part of the Bruery Platform.
+ *
+ * (c) Viktore Zara <viktore.zara@gmail.com>
+ * (c) Mell Zamorw <mellzamora@outlook.com>
+ *
+ * Copyright (c) 2016. For the full copyright and license information, please view the LICENSE  file that was distributed with this source code.
+ */
 
 namespace Bruery\AdminBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-/**
- * This is the class that validates and merges configuration from your app/config files.
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
- */
 class Configuration implements ConfigurationInterface
 {
     /**

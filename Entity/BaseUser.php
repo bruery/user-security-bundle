bb782789255a9a2dcf8c47e2d3ce6fc474238f2d
gzilt <viktore.zara@gmail.com>
2016-08-07 21:49:45 +0800

    Squashed 'src/bundles/UserBundle/' changes from 089db59..f7272a7
    f7272a7 Migrated RzUserBundle to Bruery.
    
    git-subtree-dir: src/bundles/UserBundle
    git-subtree-split: f7272a7f646f5d906753b0ae570433d7242e26b4

---
+++ Entity/BaseUser.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of the Bruery Platform.
+ *
+ * (c) Viktore Zara <viktore.zara@gmail.com>
+ * (c) Mell Zamora <mellzamora@outlook.com>
+ *
+ * Copyright (c) 2016. For the full copyright and license information, please view the LICENSE  file that was distributed with this source code.
+ */
+
+namespace Bruery\UserBundle\Entity;
+
+use Sonata\UserBundle\Entity\BaseUser as AbstractedUser;
+
+class BaseUser extends AbstractedUser
+{
+    protected $passwordChangedAt;
+
+    /**
+     * @return mixed
+     */
+    public function getPasswordChangedAt()
+    {
+        return $this->passwordChangedAt;
+    }
+
+    /**
+     * @param mixed $passwordChangedAt
+     */
+    public function setPasswordChangedAt($passwordChangedAt)
+    {
+        $this->passwordChangedAt = $passwordChangedAt;
+    }
+
+    public function setPlainPassword($password)
+    {
+        $this->setPasswordChangedAt(new \DateTime());
+        return parent::setPlainPassword($password);
+    }
+}
